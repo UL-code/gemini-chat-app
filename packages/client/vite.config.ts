bcbbe522dd974b554756b53c14b6410740403944
UL <udara.askonline@gmail.com>
2025-09-09 20:56:36 +0530

    connect the frontend and backend

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      // Redirect API calls to the backend server
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
