ad6aa5a2e3acf294810fb0d32a30d21b4852cf0d
UL <udara.askonline@gmail.com>
2025-09-12 02:11:07 +0530

    Create routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,15 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controller/chat.controller.js';
+import router from './routes.ts';
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
+app.use(router);
 
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello, World!');
-});
-
-app.post('/api/chat', chatController.sendMessage);
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
    console.log(`Server is running at http://localhost:${port}`);
